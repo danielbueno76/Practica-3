d3dbac8ce63d3f033dbf5b8f395f51e001750440
Daniel Bueno <danielbueno76@gmail.com>
2020-12-24 18:10:48 +0100

    Data structure with the first stage teams names.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 import {worldCupTeams} from './teams.js'
-import worldCupStage from './knockout/knockoutstage.js'
+import worldCupKnockout from './knockout/knockoutstage.js'
+import { LOCAL_TEAM, AWAY_TEAM } from './knockout/knockoutstage.js'
 
-const worldCup = new worldCupStage('World Cup', worldCupTeams)
+const namesStages = {
+    2: "FINAL",
+    4: "SEMIFINALES",
+    8: "CUARTOS DE FINAL",
+    16: "OCTAVOS DE FINAL",
+    32: "DIECISEISAVOS DE FINAL"
+}
+const worldCup = new worldCupKnockout('World Cup', worldCupTeams)
 
-const teamNames = worldCup.teams.map(team => team)
-console.log(teamNames)
+worldCup.scheduleStage()
+
+// Show the knockout stages although it is not mandatory. Just for clarification
+let i = worldCupTeams.length
+worldCup.matchDaysStage.forEach(matchDay => {
+    const stage = namesStages[i]
+    console.log(`===== ${stage} =====`)
+    matchDay.forEach(match => {
+        const home = match[LOCAL_TEAM]
+        const away = match[AWAY_TEAM]
+        console.log(`${home} vs ${away}`)
+    })
+    i = i /2
+})
--- knockout/knockoutstage.js
+++ knockout/knockoutstage.js
@@ -17,17 +17,48 @@ export const AWAY_TEAM = 1
 export default class KnockoutPhase {
     constructor(name, teams=[]) {
         this.name = name
-        this.matchDaySchedule = []
+        this.matchDaysStage = []
         this.setupTeams(teams)
         this.summaries = []
     }
 
     setupTeams(teamNames) {
-        this.teams = []
-        for (const teamName of teamNames) {
-            this.teams.push(teamName)
-        }
+        this.teams = teamNames
         this.teams.shuffle()
     }
 
+    scheduleStage(){
+        for (let i = this.teams.length; i > 1 ; i=i/2) {
+            const newStage = this.createStage(i/2)
+
+            this.matchDaysStage = this.matchDaysStage.concat(newStage)
+        }
+    }
+
+    createStage(numberStage) {
+        const newStage = []
+        this.initStage(newStage, numberStage)
+        if (numberStage == (this.teams.length/2))
+            this.setFirstStageTeams(newStage)
+        return newStage
+    }
+
+    initStage(newStage, numberStage) {
+        const numberOfTeamsPerStage = numberStage
+        const matchStage = []
+        for (let i = 0; i < numberOfTeamsPerStage; i++) {
+            const match = ['Local team', 'Away team']
+            matchStage.push(match)
+        }
+        newStage.push(matchStage)
+    }
+
+    setFirstStageTeams(newStage) {
+        let teamIndex = 0
+        newStage[0].forEach(match => {
+            match[LOCAL_TEAM] = this.teams[teamIndex]
+            match[AWAY_TEAM] = this.teams[++teamIndex]
+            teamIndex++
+        })
+    }
 }
